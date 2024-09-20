1825d2188b4cef01ff8d7e5e08bf1911d12e513f
Julien Boriasse <julien@boriasse.com>
2024-09-20 11:07:30 -0400

    API data

--- include/arduino_secrets.h
+++ include/arduino_secrets.h
@@ -2,3 +2,6 @@
   #define EAP_USERNAME "abc@cmaisonneuve.qc.ca"
   #define EAP_PASSWORD "123456"
   #define SECRET_SSID "eduroam"
+
+  #define API_KEY "123456"
+  #define API_HOST "api.simplyprint.io/12305/endpoint/"
