77922426a332e112740cef1d30746ec36c473c2e
Test <Test>
2024-11-07 17:47:25 -0500

    SSL fail

--- src/main.cpp
+++ src/main.cpp
@@ -46,11 +46,87 @@ void setup()
   Serial.println(WiFi.SSID());
 }
 
+const String discord_webhook = SECRET_WEBHOOK;
+const String discord_tts = SECRET_TTS;
+//openssl s_client -showcerts -connect discordapp.com:443 (get last certificate)
+const char* discordappCertificate = \
+                                    "-----BEGIN CERTIFICATE-----\n"
+                                    "MIIDrDCCA1KgAwIBAgIQI7XTQ9dw6e4Rt7Am9U6u/DAKBggqhkjOPQQDAjA7MQsw\n"
+                                    "CQYDVQQGEwJVUzEeMBwGA1UEChMVR29vZ2xlIFRydXN0IFNlcnZpY2VzMQwwCgYD\n"
+                                    "VQQDEwNXRTEwHhcNMjQwOTE5MDMzNTE5WhcNMjQxMjE4MDMzNTE4WjAZMRcwFQYD\n"
+                                    "VQQDEw5kaXNjb3JkYXBwLmNvbTBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABHBk\n"
+                                    "gfnSs5c7xWBslQrPURj2HJWASNSi68BYs8EEsePKQ/PITPE+EQS4w6+sDQZASSN9\n"
+                                    "RZq2zFM0kK2TrDVOCqmjggJYMIICVDAOBgNVHQ8BAf8EBAMCB4AwEwYDVR0lBAww\n"
+                                    "CgYIKwYBBQUHAwEwDAYDVR0TAQH/BAIwADAdBgNVHQ4EFgQUuhBvie/2+rVJG2ft\n"
+                                    "AlrxV+xw0bEwHwYDVR0jBBgwFoAUkHeSNWfE/6jMqeZ72YB5e8yT+TgwXgYIKwYB\n"
+                                    "BQUHAQEEUjBQMCcGCCsGAQUFBzABhhtodHRwOi8vby5wa2kuZ29vZy9zL3dlMS9J\n"
+                                    "N1UwJQYIKwYBBQUHMAKGGWh0dHA6Ly9pLnBraS5nb29nL3dlMS5jcnQwKwYDVR0R\n"
+                                    "BCQwIoIOZGlzY29yZGFwcC5jb22CECouZGlzY29yZGFwcC5jb20wEwYDVR0gBAww\n"
+                                    "CjAIBgZngQwBAgEwNgYDVR0fBC8wLTAroCmgJ4YlaHR0cDovL2MucGtpLmdvb2cv\n"
+                                    "d2UxL3Q2MkV0NFdFNVFZLmNybDCCAQMGCisGAQQB1nkCBAIEgfQEgfEA7wB2AHb/\n"
+                                    "iD8KtvuVUcJhzPWHujS0pM27KdxoQgqf5mdMWjp0AAABkgiQMYMAAAQDAEcwRQIg\n"
+                                    "MkOv7ZZuNlXHCA/y+IICny741JdghaWn6aXo1fs+xhgCIQDB0vdMwciEOLvSBF2Q\n"
+                                    "GuwKXMGnM01DN/+Lf6D0cZIK3AB1AD8XS0/XIkdYlB1lHIS+DRLtkDd/H4Vq68G/\n"
+                                    "KIXs+GRuAAABkgiQMWQAAAQDAEYwRAIgBq7SoQxfguTVwUCQboRGqVF401VwTN7x\n"
+                                    "dXj+bJAxN4wCIEBUoJLq/fZB/yl1dkWq50AZePstlWZ93L1Q/3gbjql9MAoGCCqG\n"
+                                    "SM49BAMCA0gAMEUCIQDUR1jLiaP1fyCxL0ma/AYP2vsiOmN4tlw3P9sHMV5M1gIg\n"
+                                    "TdSDVN1MDuXjS2q0J2bu+LRB59b7zabDGs7K7/nORWo=\n"
+                                    "-----END CERTIFICATE-----\n";
+
+void sendDiscord(String content) {
+  WiFiClientSecure *client = new WiFiClientSecure;
+  if (client) {
+    client -> setCACert(discordappCertificate);
+    {
+      HTTPClient https;
+      Serial.println("[HTTP] Connecting to Discord...");
+      Serial.println("[HTTP] Message: " + content);
+      Serial.println("[HTTP] TTS: " + discord_tts);
+      if (https.begin(*client, discord_webhook)) {  // HTTPS
+        // start connection and send HTTP header
+        https.addHeader("Content-Type", "application/json");
+        int httpCode = https.POST("{\"content\":\"" + content + "\",\"tts\":" + discord_tts +"}");
+
+        // httpCode will be negative on error
+        if (httpCode > 0) {
+          // HTTP header has been send and Server response header has been handled
+          Serial.print("[HTTP] Status code: ");
+          Serial.println(httpCode);
+
+          // file found at server
+          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+            String payload = https.getString();
+            Serial.print("[HTTP] Response: ");
+            Serial.println(payload);
+          }
+        } else {
+          Serial.print("[HTTP] Post... failed, error: ");
+          Serial.println(https.errorToString(httpCode).c_str());
+        }
+
+        https.end();
+      } else {
+        Serial.printf("[HTTP] Unable to connect\n");
+      }
+
+      // End extra scoping block
+    }
+
+    delete client;
+  } else {
+    Serial.println("[HTTP] Unable to create client");
+  }
+}
+
 void loop()
 {
   if (WiFi.status() == WL_CONNECTED)
   {
+    sendDiscord("Hello World!");
+    
     HTTPClient http;
+
+    
 
     // Spécification de l'URL
     http.begin("https://api.simplyprint.io/12305/printers/Get");
